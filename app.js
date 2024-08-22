b66e821c6d1e91bcbe4e47eb14eb03fa2eee219c
msavaizsheikh <155227408+msavaizsheikh@users.noreply.github.com>
2024-08-22 12:41:31 +0500

    js

---
+++ app.js
@@ -0,0 +1,14 @@
+ const myname = prompt("what is your name")
+ console.log(myname)
+
+const a = ["ali", , "murad"];
+console.log(0);
+
+const b = "34"
+parseInt(b)
+console.log(typeof b);
+
+const c = "54"
+const d = "45"
+
+console.log(c,d)
